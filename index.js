9476b7fbd2f036e8af3301dcc4dc97c8d1458041
Harriet Kerubo <keruboharriet7@gmail.com>
2023-11-29 04:03:42 -0500

    increasing likes count with js

--- index.js
+++ index.js
@@ -1,65 +1,57 @@
-// const userInfoEl = document.querySelector(".user-info");
-// const infoEl = document.querySelector(".info");
-// const postImageEl = document.querySelector(".post-image");
-// const likeCountEl = document.querySelector(".like-count");
-// const captionEl = document.querySelector(".caption");
-// const postContentEl = document.querySelector(".post-content");
-const mainSectiontEl = document.querySelector(".main");
+document.addEventListener("DOMContentLoaded", function () {
+  const mainSectiontEl = document.querySelector(".main");
+  const posts = [
+    {
+      name: "Vincent van Gogh",
+      username: "vincey1853",
+      location: "Zundert, Netherlands",
+      avatar: "images/avatar-vangogh.jpg",
+      post: "images/post-vangogh.jpg",
+      comment: "just took a few mushrooms lol",
+      likes: 21,
+    },
+    {
+      name: "Gustave Courbet",
+      username: "gus1819",
+      location: "Ornans, France",
+      avatar: "images/avatar-courbet.jpg",
+      post: "images/post-courbet.jpg",
+      comment: "i'm feelin a bit stressed tbh",
+      likes: 4,
+    },
+    {
+      name: "Joseph Ducreux",
+      username: "jd1735",
+      location: "Paris, France",
+      avatar: "images/avatar-ducreux.jpg",
+      post: "images/post-ducreux.jpg",
+      comment:
+        "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
+      likes: 152,
+    },
+  ];
 
-console.log(mainSectiontEl);
+  let name = "";
+  let username = "";
+  let userlocation = "";
+  let avatar = "";
+  let post = "";
+  let comment = "";
+  let likes = "";
 
-const posts = [
-  {
-    name: "Vincent van Gogh",
-    username: "vincey1853",
-    location: "Zundert, Netherlands",
-    avatar: "images/avatar-vangogh.jpg",
-    post: "images/post-vangogh.jpg",
-    comment: "just took a few mushrooms lol",
-    likes: 21,
-  },
-  {
-    name: "Gustave Courbet",
-    username: "gus1819",
-    location: "Ornans, France",
-    avatar: "images/avatar-courbet.jpg",
-    post: "images/post-courbet.jpg",
-    comment: "i'm feelin a bit stressed tbh",
-    likes: 4,
-  },
-  {
-    name: "Joseph Ducreux",
-    username: "jd1735",
-    location: "Paris, France",
-    avatar: "images/avatar-ducreux.jpg",
-    post: "images/post-ducreux.jpg",
-    comment:
-      "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
-    likes: 152,
-  },
-];
+  let postContentItems = "";
 
-let name = "";
-let username = "";
-let userlocation = "";
-let avatar = "";
-let post = "";
-let comment = "";
-let likes = "";
+  for (let i = 0; i < posts.length; i++) {
+    postContent = posts[i];
+    name = postContent.name;
+    username = postContent.username;
+    userlocation = postContent.location;
+    avatar = postContent.avatar;
+    post = postContent.post;
+    comment = postContent.comment;
+    likes = postContent.likes;
 
-let postContentItems = "";
-
-for (let i = 0; i < posts.length; i++) {
-  postContent = posts[i];
-  name = postContent.name;
-  username = postContent.username;
-  userlocation = postContent.location;
-  avatar = postContent.avatar;
-  post = postContent.post;
-  comment = postContent.comment;
-  likes = postContent.likes;
-
-  postContentItems += ` 
+    postContentItems += ` 
   <div class= "post-content">
   <div class="user-info">
   <img class="avatar" src=${avatar}/>
@@ -75,12 +67,12 @@ for (let i = 0; i < posts.length; i++) {
 </div>
 <div class="post-body">
 <div class="icons">
-  <img class="post-icons" src="images/icon-heart.png" />
+  <img class="like-icon post-icons" src="images/icon-heart.png" />
   <img class="post-icons" src="images/icon-comment.png" />
   <img class="post-icons" src="images/icon-comment.png" />
 </div>
 <div class="like-count">
-  <h3> ${likes}<span class="likes"> likes </span></h3>
+ <h3><span class="likes">${likes}</span> likes</h3>
 </div>
 <div class="caption">
   <h3>
@@ -92,6 +84,19 @@ for (let i = 0; i < posts.length; i++) {
 </div>
 
   `;
-}
+  }
 
-mainSectiontEl.innerHTML = postContentItems;
+  mainSectiontEl.innerHTML = postContentItems;
+
+  const likeIconEls = document.querySelectorAll(".like-icon");
+  const likeCountEls = document.querySelectorAll(".like-count");
+
+  likeIconEls.forEach(function (likeIconEl, index) {
+    likeIconEl.addEventListener("click", function () {
+      const likesContainer = likeCountEls[index].querySelector(".likes");
+      let likesValue = parseInt(likesContainer.textContent);
+      likesValue += 1;
+      likesContainer.textContent = likesValue;
+    });
+  });
+});
